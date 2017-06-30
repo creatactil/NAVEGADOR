40c09583d4fb5f95cb0e7a58df671ba7bfc97416
creatactil <venazcocabrera@gmail.com>
2017-06-30 13:40:55 +0100

    INICIO

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-themeablebrowser.themeablebrowser",
+        "file": "plugins/cordova-plugin-themeablebrowser/www/themeablebrowser.js",
+        "pluginId": "cordova-plugin-themeablebrowser",
+        "clobbers": [
+            "cordova.ThemeableBrowser"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-themeablebrowser": "0.2.17"
+};
+// BOTTOM OF METADATA
+});
