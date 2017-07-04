a13a651647fa4277eb487d1974025d1892b7c2f3
creatactil <venazcocabrera@gmail.com>
2017-07-04 11:17:25 +0100

    scan01

---
+++ www/js/escanear.js
@@ -0,0 +1,13 @@
+function escanear(){
+			
+	var callback = function(err, contents){
+	 
+	  if(err){
+		console.error(err._message);
+	  }
+	  alert('The QR Code contains: ' + contents);
+	};
+	 
+	QRScanner.scan(callback);	
+	
+}
